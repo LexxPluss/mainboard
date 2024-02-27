859a597ec654cea5e6266d412d0a48fbb35447ac
Takuro Tsujikawa <takuro.tsujikawa@gmail.com>
2024-02-27 10:38:19 +0900

    added retry init with decriment duty

--- lexxpluss_apps/src/actuator_controller.cpp
+++ lexxpluss_apps/src/actuator_controller.cpp
@@ -496,9 +496,11 @@ public:
     int init_location() {
         LOG_INF("initialize location.");
         location_initialized = false;
-        pwm_trampoline_all(msg_control::DOWN, 100);
-        bool stopped{wait_actuator_stop(30000, 100)};
-        pwm_trampoline_all(msg_control::STOP);
+        for(uint32_t ii{0}; ii < 5; ++ii){
+            pwm_trampoline_all(msg_control::DOWN, (100 - ii * 5));
+            bool stopped{wait_actuator_stop(30000, 100)};
+            pwm_trampoline_all(msg_control::STOP);
+        }
         if (!stopped || can_controller::is_emergency()) {
             LOG_WRN("can not initialize location.");
             return -1;
